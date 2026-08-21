bcad4f74e419508be3e6a111ff21c977c324fcec
mahnoor-hub-ai <mahnoorasif.hub@gmail.com>
2026-08-21 16:17:40 +0500

    Add Lecture 05 - Operators

---
+++ Table of private/public/protected members.cpp
@@ -0,0 +1,12 @@
+   /*               {Table  of  Derivations}
+
+
+                            private derivation      protected derivation     public derivation
+
+    1..private   members         NOT                     NOT                    NOT
+    2..protected members       PRIVATE                 PROTECTED                NOT
+    3..public    members       PUBLIC                  PROTECTED               PUBLIC
+
+
+
+*/
